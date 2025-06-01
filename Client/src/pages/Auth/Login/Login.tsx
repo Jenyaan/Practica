a650c3de3fa8c37b7bd5998c6c2221fa6b30d4e5
Jenyaan <jekaniks34@gmail.com>
2025-06-01 16:22:44 +0300

    axios auth

--- Client/src/pages/Auth/Login/Login.tsx
+++ Client/src/pages/Auth/Login/Login.tsx
@@ -1,10 +1,31 @@
 import styles from '../../../overlays/Auth/Auth.module.css';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import type { LoginRequest } from '../../../models/AuthModel';
+import axios from 'axios';
+import { useState } from 'react';
+import { PREFIX } from '../../../api/API';
 
 const Login = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = () => console.log("d"); 
+  const { register, handleSubmit, formState: { errors } } = useForm<LoginRequest>();
+  const [serverMessage, setServerMessage] = useState('');
+
+  
+  const onSubmit = async (data: LoginRequest) => {
+  try {
+    const response = await axios.post(`${PREFIX}/api/v1/auth/login`, data);
+
+    if (response.status === 200) {
+      setServerMessage('Вхід успішний!');
+    }
+  } catch (error: any) {
+    if (error.response?.status === 401) {
+      setServerMessage('Невірний логін або пароль.');
+    } else {
+      setServerMessage('Сталася помилка. Спробуйте пізніше.');
+    }
+  }
+};
 
   return (
     <>
@@ -17,15 +38,15 @@ const Login = () => {
           {...register("password", { required: "Пароль обов’язковий", minLength: { value: 6, message: "Мінімум 6 символів" } })} 
           type="password" placeholder="Пароль" className={styles.auth_input} />
 
-        {(errors.email || errors.password) && (
-          <p className={styles.auth_error}>
-            {typeof errors.email?.message === "string"
-              ? errors.email.message
-              : typeof errors.password?.message === "string"
-              ? errors.password.message
-              : ""}
-          </p>
-        )}
+          {(errors.email || errors.password || serverMessage) && (
+            <p className={styles.auth_error}>
+              {typeof errors.email?.message === "string"
+                ? errors.email.message
+                : typeof errors.password?.message === "string"
+                ? errors.password.message
+                : serverMessage}
+            </p>
+          )}
 
         <button type="submit" className={styles.auth_button}>Увійти</button>
       </form>
--- Client/src/pages/Auth/Registrate/Registrate.tsx
+++ Client/src/pages/Auth/Registrate/Registrate.tsx
@@ -1,12 +1,39 @@
 import styles from '../../../overlays/Auth/Auth.module.css';
 import { Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import type { RegistrateRequest } from '../../../models/AuthModel';
+import axios from 'axios';
+import { PREFIX } from '../../../api/API';
 
 const Registrate = () => {
   const { register, handleSubmit, watch, formState: { errors } } = useForm();
   const password = watch('password');
-  const onSubmit = (data: any) => console.log(data);
+  const [serverMessage, setServerMessage] = useState('');
 
+  
+  const onSubmit = async (dataF: any) => {
+
+    const data: RegistrateRequest = {
+      name: dataF.name,
+      email: dataF.email,
+      password: dataF.password
+    }
+    
+    try {
+      const response = await axios.post(`${PREFIX}/api/v1/users`, data);
+
+      if (response.status === 200) {
+        setServerMessage('Вхід успішний!');
+      }
+    } catch (error: any) {
+      if (error.response?.status === 401) {
+        setServerMessage('Невірний логін або пароль.');
+      } else {
+        setServerMessage('Сталася помилка. Спробуйте пізніше.');
+      }
+    }
+  };
   return (
     <>
       <h1 className={styles.auth_title}>Реєстрація</h1>
@@ -36,7 +63,7 @@ const Registrate = () => {
               ? errors.password.message
               : typeof errors.name?.message === "string"
               ? errors.name.message
-              : ""}
+              : serverMessage}
           </p>
         )}        
         
